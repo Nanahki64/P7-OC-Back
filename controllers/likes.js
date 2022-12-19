c05021f93755267fb65d1e4cbbd70ad70565063d
clement.lafon <clement.lafon@comwatt.com>
2022-12-19 16:07:25 +0100

    Implement likes controller and route

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,14 +1,73 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-exports.createLike = (req, res , next) => {
-    prisma.likes.create({
-        data: {
-            author: { connect: { id: req.auth.userId } },
-            post: { connect:  { id: req.body.postId } },
-            like: req.body.like 
+exports.createOrUpdate = async (req, res , next) => {
+    try {
+        const [like, count] = await prisma.$transaction(
+        [ 
+            prisma.likes.upsert({
+                where: { 
+                    postId_authorId: { 
+                        authorId: req.auth.userId,
+                        postId: req.body.postId
+                    }
+                },
+                create: {
+                    author: { connect: { id: req.auth.userId } },
+                    post: { connect:  { id: req.body.postId } },
+                    like: req.body.like
+                },
+                update: {
+                    like: req.body.like
+                },
+                select: {
+                    like: true
+                }
+            }),
+            prisma.likes.count({
+                where: {
+                    postId: req.body.postId
+                }
+            })
+        ]
+        );
+        res.status(201).json({like, count});
+    } catch(e) {
+        res.status(400).json({ message: `postCreate failed: ${e}` });
+    }
+}
+
+exports.deleteLike = async (req, res , next) => {
+    try {
+        const count = await prisma.$transaction(
+        [ 
+            prisma.likes.delete({
+                where: { 
+                    postId_authorId: { 
+                        authorId: req.auth.userId,
+                        postId: req.body.postId
+                    }
+                }
+            }),
+            prisma.likes.count({
+                where: {
+                    postId: req.body.postId
+                }
+            })
+        ]
+        );
+        res.status(201).json({count});
+    } catch(e) {
+        res.status(400).json({ message: `deleteLike failed: ${e}` });
+    }
+}
+
+exports.getLikesCount = (req, res, next) => {
+    prisma.likes.count({
+        where: {
+            postId: req.params.id
         }
     })
-    .then((post) => res.status(200).json({ post }))
-    .catch((e) => res.status(400).json({ message: `postCreate failed: ${e}` }));
+    .then((likes) => res.status(200).json({likes}))
+    .catch(() => res.status(400).json({ message: 'erreur: impossible de recuperer le nombre de likes.' }))
 }
--- routes/likes.js
+++ routes/likes.js
@@ -4,8 +4,18 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 /**
- * 
+ * Crée ou met un jour un like.
  */
-router.post('/', auth, likesCtrl.createLike);
+router.post('/', auth, likesCtrl.createOrUpdate);
+
+/**
+ * Supprime le like d'un utilisateur.
+ */
+router.delete('/', auth, likesCtrl.deleteLike);
+
+/**
+ * Récupère le nombre de like d'un post.
+ */
+router.get('/:id', auth, likesCtrl.getLikesCount);
 
 module.exports = router;
